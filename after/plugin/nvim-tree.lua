06a28888a1514dc1ddbfe5488584b47258e2ffdb
Francisco Veiras <74626997+francisco-svg761@users.noreply.github.com>
2023-04-25 22:12:24 -0300

    packer_init changes

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -4,13 +4,11 @@ vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 
 tree.setup {
+  on_attach = on_attach,
   sort_by = "case_sensitive",
   view = {
     adaptive_size = true,
     mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-      },
     },
   },
   renderer = {
--- lua/packer_init.lua
+++ lua/packer_init.lua
@@ -3,9 +3,17 @@ local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  execute('!git clone https://github.com/wbthomason/packer.nvim.git '..install_path)
-  execute 'packadd packer.nvim'
+  PACKER_BOOTSTRAP = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  print("Installing packer close and reopen Neovim...")
 end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
 
 Packer = require'packer'
 
